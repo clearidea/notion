dc7ed37d692697987e5037b190cb7e48a4479172
Lee Jones <lee@dragonflyrg.com>
2020-09-04 12:13:06 -0400

    Fix to addRoute.

--- src/Notion/Route.php
+++ src/Notion/Route.php
@@ -4,28 +4,28 @@ namespace Notion;
 
 class Route
 {
-	public static function delete( string $Route, $Function ) : RouteMap
+	public static function delete( string $Route, $Function ) : Router
 	{
 		$Router = Router::getInstance();
 
 		return $Router->delete( $Route, $Function );
 	}
 
-	public static function get( string $Route, $Function ) : RouteMap
+	public static function get( string $Route, $Function ) : Router
 	{
 		$Router = Router::getInstance();
 
 		return $Router->get( $Route, $Function );
 	}
 
-	public static function post( string $Route, $Function ) : RouteMap
+	public static function post( string $Route, $Function ) : Router
 	{
 		$Router = Router::getInstance();
 
 		return $Router->post( $Route, $Function );
 	}
 
-	public static function put( string $Route, $Function ) : RouteMap
+	public static function put( string $Route, $Function ) : Router
 	{
 		$Router = Router::getInstance();
 
